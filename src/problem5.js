1391e6ec4164b66132cd927bcdd8d7cf8a5497fc
seowoojin <woody306@naver.com>
2025-10-05 21:39:36 +0900

    problem 5 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [];
+
+  for (let unit of units) {
+    const count = Math.floor(money / unit);
+    result.push(count);
+    money %= unit;
+  }
+  
+  return result;
 }
 
 module.exports = problem5;
