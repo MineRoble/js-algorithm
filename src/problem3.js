6975c5cabbed25c30c40cbd107aed1aaa81cef50
seowoojin <woody306@naver.com>
2025-10-05 21:36:47 +0900

    problem 3 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const str = String(i);
+    for (let item of str) {
+      if (item === "3" || item === "6" || item === "9") {
+        answer++;
+      }
+    }
+  }
+
   return answer;
 }
 
