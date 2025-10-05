8b9b28a4a6be434162b327624fd701d4db778073
seowoojin <woody306@naver.com>
2025-10-05 21:33:22 +0900

    problem 2 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let changed = true;
+
+  while (changed) {
+    changed = false;
+    let result = "";
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (cryptogram[i] === cryptogram[i + 1]) {
+        changed = true;
+        i++;
+      } else {
+        result += cryptogram[i];
+      }
+    }
+    cryptogram = result;
+  }
+
+  return cryptogram;
 }
 
 module.exports = problem2;
