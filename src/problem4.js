f199fe1762bc666be917c3a755fd27dc157aa08e
seowoojin <woody306@naver.com>
2025-10-05 21:38:30 +0900

    problem 4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = '';
+
+  for (let item of word) {
+    if (item >= 'A' && item <= 'Z') {
+      result += String.fromCharCode('Z'.charCodeAt(0) - (item.charCodeAt(0) - 'A'.charCodeAt(0)));
+    } else if (item >= 'a' && item <= 'z') {
+      result += String.fromCharCode('z'.charCodeAt(0) - (item.charCodeAt(0) - 'a'.charCodeAt(0)));
+    } else {
+      result += item;
+    }
+  }
+
+  return result;
 }
 
 module.exports = problem4;
